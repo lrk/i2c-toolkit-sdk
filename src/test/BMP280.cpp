c71e783ad5842199978af77463a80419660dff69
Elrick <sfxelrick@gmail.com>
2017-02-04 23:23:10 +0100

    commit

--- src/test/BMP280.cpp
+++ src/test/BMP280.cpp
@@ -31,7 +31,7 @@ int main()
 	BMP280 *bmp280 = new BMP280(0x76,RPI_IO);
 
 	cout << "Full reset of the Device" << endl;
-	bmp280->reset(true);
+	//bmp280->reset(true);
 	cout << "Initializing Device" << endl;
 	bmp280->initialize();
 
@@ -43,32 +43,33 @@ int main()
 	BMP280_CALIBRATION calibration = bmp280->readCalibration();
 
 	cout << "Reading Calibration: " << endl;
-	cout << " T1: " <<  hex << setw(2) <<  (int) calibration.digT1;
-	cout << " T2: " <<  hex << setw(2) <<  (int) calibration.digT2;
-	cout << " T3: " <<  hex << setw(2) <<  (int) calibration.digT3;
+	cout << " T1: " <<  hex << setw(2) <<  (uint16_t) calibration.digT1;
+	cout << " T2: " <<  hex << setw(2) <<  (int16_t) calibration.digT2;
+	cout << " T3: " <<  hex << setw(2) <<  (int16_t) calibration.digT3;
 
 	cout << endl;
 
-	cout << " P1: " <<  hex << setw(2) <<  (int) calibration.digP1;
-	cout << " P2: " <<  hex << setw(2) <<  (int) calibration.digP2;
-	cout << " P3: " <<  hex << setw(2) <<  (int) calibration.digP3;
+	cout << " P1: " <<  hex << setw(2) <<  (uint16_t) calibration.digP1;
+	cout << " P2: " <<  hex << setw(2) <<  (int16_t) calibration.digP2;
+	cout << " P3: " <<  hex << setw(2) <<  (int16_t) calibration.digP3;
 	cout << endl;
-	cout << " P4: " <<  hex << setw(2) <<  (int) calibration.digP4;
-	cout << " P5: " <<  hex << setw(2) <<  (int) calibration.digP5;
-	cout << " P6: " <<  hex << setw(2) <<  (int) calibration.digP6;
+	cout << " P4: " <<  hex << setw(2) <<  (int16_t) calibration.digP4;
+	cout << " P5: " <<  hex << setw(2) <<  (int16_t) calibration.digP5;
+	cout << " P6: " <<  hex << setw(2) <<  (int16_t) calibration.digP6;
 	cout << endl;
-	cout << " P7: " <<  hex << setw(2) <<  (int) calibration.digP7;
-	cout << " P8: " <<  hex << setw(2) <<  (int) calibration.digP8;
-	cout << " P9: " <<  hex << setw(2) <<  (int) calibration.digP9;
+	cout << " P7: " <<  hex << setw(2) <<  (int16_t) calibration.digP7;
+	cout << " P8: " <<  hex << setw(2) <<  (int16_t) calibration.digP8;
+	cout << " P9: " <<  hex << setw(2) <<  (int16_t) calibration.digP9;
 	cout << endl;
 
-	cout << "Looping through reading T° & hPa (quit: press space key). " << endl;
+	cout << "Looping through reading T° & hPa" << endl;
 	do{
 		bmp280->controlMeasure(SKIPPED,SKIPPED,FORCED);
 		sleep(1);
     	cout << "Reading Temperature: " << bmp280->temperature() << endl;
     	cout << "Reading Pressure: " << bmp280->pressure() << endl;
     	sleep(1);
+    	cout << "Space to quit, Enter to continue" << endl;
     }
     while(getchar() != 32 || getchar() != ' ');
     
